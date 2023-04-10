bc05b4777fcc92ed6517974dac6261b6c487fee6
mo <103134705+pm888@users.noreply.github.com>
2023-04-10 12:32:53 +0700

    v1.2.1

--- cmd/internal/server/server.go
+++ cmd/internal/server/server.go
@@ -2,31 +2,15 @@ package server
 
 import (
 	Serviceuser "Twitter_like_application/cmd/internal/database"
+	"Twitter_like_application/cmd/internal/services"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
-var tweets []Serviceuser.Tweet
-
 func Server() {
 	r := mux.NewRouter()
-	//r.HandleFunc("/tweets", getTweets).Methods("GET")
-	r.HandleFunc("/tweets", Serviceuser.CreateUser).Methods("POST")
+	r.HandleFunc("/tweets", services.GetTweets).Methods("GET")
+	r.HandleFunc("/tweets", services.CreateTweet).Methods("POST")
+	r.HandleFunc("/addusers", Serviceuser.CreateUser).Methods("POST")
 	http.ListenAndServe("localhost:8080", r)
 }
-
-//func getTweets(w http.ResponseWriter, r *http.Request) {
-//	w.Header().Set("Content-Type", "application/json")
-//	json.NewEncoder(w).Encode(tweets)
-//}
-
-//func createTweet(w http.ResponseWriter, r *http.Request) {
-//	var tweet Serviceuser.Tweet
-//	_ = json.NewDecoder(r.Body).Decode(&tweet)
-//	tweet.ID = time.Now().Format("20060102150405")
-//	tweet.CreatedAt = time.Now()
-//	tweets = append(tweets, tweet)
-//	w.Header().Set("Content-Type", "application/json")
-//	w.WriteHeader(http.StatusCreated)
-//	json.NewEncoder(w).Encode(tweet)
-//}
