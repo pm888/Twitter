ee0327ccbc0e1fd257035ef3f7d04f4730914b41
mo <103134705+pm888@users.noreply.github.com>
2023-07-06 15:09:04 +0700

    TWI-11 Get current profile
    Move: GetCurrentProfile in a separate file
    Change: get current profile, *.yaml for Get Current User Profile.
    Add: GetCurrentUser struct

--- internal/users/usersStr.go
+++ internal/users/usersStr.go
@@ -20,6 +20,15 @@ type Users struct {
 	Following []int
 	Followers []int
 }
+type GetCurrentUser struct {
+	Name      string `json:"name"`
+	BirthDate string `json:"birthdate"`
+	Nickname  string `json:"nickname"`
+	Bio       string `json:"bio"`
+	Location  string `json:"location"`
+	Following int    `json:"following"`
+	Followers int    `json:"followers"`
+}
 
 type ReplaceMyData struct {
 	NewName      string `json:"new_name"`
